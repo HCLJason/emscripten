0025c37de82f7b7bfce86e7c6c1e5b90721ae9e3
HCLJason <79225526+HCLJason@users.noreply.github.com>
2022-10-21 13:27:18 -0400

    Don't need shutil

--- tools/ports/zlib.py
+++ tools/ports/zlib.py
@@ -4,7 +4,6 @@
 # found in the LICENSE file.
 
 import os
-import shutil
 
 VERSION = '1.2.13'
 HASH = '44b834fbfb50cca229209b8dbe1f96b258f19a49f5df23b80970b716371d856a4adf525edb4c6e0e645b180ea949cb90f5365a1d896160f297f56794dd888659'
